b5d39fb815dbc9437b2fa51bce2b5505e3ea26df
Tim Mingxing <tim.tt24@gmail.com>
2020-07-30 16:23:09 +0800

    抽奖脚本取消用数据库保存信息，兼容云函数

--- BiliLottery.py
+++ BiliLottery.py
@@ -1,28 +1,9 @@
+# -*- coding: utf-8 -*-
 from models.Biliapi import BiliWebApi
-import sqlite3
-
-cookieData = {
-    "SESSDATA": "",
-    "bili_jct": "",
-    "DedeUserID": "",
-}
-
-def update(dynamic_id):
-    conn = sqlite3.connect('lottery.db')
-    cursor = conn.cursor()
-    cursor.execute("SELECT id FROM lottery WHERE dynamic_id=?", (dynamic_id,))
-    result = True if cursor.fetchone() else False
-    if not result:
-        cursor.execute("SELECT id FROM lottery ORDER BY date ASC LIMIT 0,1")
-        id = cursor.fetchone()[0]
-        cursor.execute("UPDATE lottery SET dynamic_id=? WHERE id=?", (dynamic_id, id))
-    cursor.close()
-    conn.commit()
-    conn.close()
-    return result
+import logging
+from userData.userData import cookieDatas
 
 def bili_lottery(data):
-
     try:
         biliapi = BiliWebApi(data)
     except Exception as e: 
@@ -30,19 +11,24 @@ def bili_lottery(data):
         return
 
     try:
-        datas = biliapi.getDynamicNew()["data"]["cards"]
+        datas = biliapi.getDynamicNew()["data"]["cards"] #获取"当前"的动态列表，不追溯历史动态
     except Exception as e: 
         logging.warning(f'获取动态列表异常，原因为{str(e)}，跳过后续所有操作')
         return
-
+    
+    already_repost_dyid = [] #记录动态列表中自己已经转发的动态id
     for x in datas:
-        if x.__contains__("extension") and x["extension"].__contains__("lott"):
-            uname = x["desc"]["user_profile"]["info"]["uname"]
-            dynamic_id = x["desc"]["dynamic_id"]
-            if not update(dynamic_id):
+        if str(x["desc"]["uid"]) == data["DedeUserID"] and x["desc"]["pre_dy_id"]: 
+            already_repost_dyid.append(x["desc"]["pre_dy_id"])
+            continue
+
+        if x.__contains__("extension") and x["extension"].__contains__("lott"): #若抽奖标签存在
+            uname = x["desc"]["user_profile"]["info"]["uname"]  #动态的主人的用户名
+            dynamic_id = x["desc"]["dynamic_id"]  #动态id
+            if not dynamic_id in already_repost_dyid: #若动态没被转发过
                 try:
                     biliapi.repost(dynamic_id)
-                    logging.info(f'转发抽奖(用户名:{uname},dynamic_id:{str(dynamic_id)})成功')
+                    logging.info(f'id为{data["DedeUserID"]}的账户转发抽奖(用户名:{uname},动态id:{str(dynamic_id)})成功')
                 except Exception as e: 
                     logging.warning(f'此次转发抽奖失败，原因为{str(e)}')
 
@@ -51,6 +37,8 @@ def main(*args):
         logging.basicConfig(filename="lottery.log", filemode='a', level=logging.INFO, format="%(asctime)s: %(levelname)s, %(message)s", datefmt="%Y/%d/%m %H:%M:%S")
     except:
         pass
-    bili_lottery(cookieData)
+
+    for x in cookieDatas:
+        bili_lottery(x)
 
 main()
