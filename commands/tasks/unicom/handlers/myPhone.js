05479d277b337b1cfdc0dbfa1b56e1a1d74120db
ytgo <39256100+ytgo@users.noreply.github.com>
2021-02-22 21:53:23 -0600

    Update myPhone.js
    Redmi K20

--- commands/tasks/unicom/handlers/myPhone.js
+++ commands/tasks/unicom/handlers/myPhone.js
@@ -30,6 +30,7 @@ const USER_AGENTS_ANDROID = [
   "Mozilla/5.0 (Linux; Android 10; CLT-AL00 Build/HUAWEICLT-AL00; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/76.0.3809.89 Mobile Safari/537.36; unicom{version:android@%s,desmobile:%s};devicetype{deviceBrand:HUAWEI,deviceModel:HUAWEICLT-AL00};{yw_code:}",
   "Mozilla/5.0 (Linux; Android 8.1.0; PBAM00 Build/OPM1.171019.026; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/76.0.3809.89 Mobile Safari/537.36; unicom{version:android@%s,desmobile:%s};devicetype{deviceBrand:OPPO,deviceModel:PBAM00};{yw_code:}", //OPPO A5
   "Mozilla/5.0 (Linux; Android 11; MI 9 Build/RKQ1.200826.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/87.0.4280.141 Mobile Safari/537.36; unicom{version:android@%s,desmobile:%s};devicetype{deviceBrand:Xiaomi,deviceModel:MI 9};{yw_code:}", //XiaoMi 9
+  "Mozilla/5.0 (Linux; Android 11; Redmi K20 Build/RKQ1.200826.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/87.0.4280.141 Mobile Safari/537.36; unicom{version:android@%s,desmobile:%s};devicetype{deviceBrand:Xiaomi,deviceModel:Redmi K20};{yw_code:}",//Redmi K20
 ];
 // eslint-disable-next-line no-unused-vars
 const USER_AGENTS_IOS = [
