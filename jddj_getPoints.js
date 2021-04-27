de9eec54f3554e1e2ad15dc8595ec9ab9c53f63f
passerby-b <40885345+passerby-b@users.noreply.github.com>
2021-04-27 13:12:39 +0800

    Update jddj_getPoints.js

--- jddj_getPoints.js
+++ jddj_getPoints.js
@@ -6,7 +6,7 @@
 //boxjs订阅地址:https://gitee.com/passerby-b/javascript/raw/master/JD/passerby-b.boxjs.json
 
 //[task_local]
-//*/5 * * * * https://gitee.com/passerby-b/javascript/raw/master/JD/jddj_getPoints.js
+//*/5 * * * * https://raw.githubusercontent.com/passerby-b/JDDJ/main/jddj_getPoints.js
 
 const $ = new API("jddj_getPoints");
 let cookies = [];
