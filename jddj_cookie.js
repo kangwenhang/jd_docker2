645ae91dd1852cdaa2114b97af8fad638c7ff20b
passerby-b <40885345+passerby-b@users.noreply.github.com>
2021-05-11 11:28:28 +0800

    Update jddj_cookie.js

--- jddj_cookie.js
+++ jddj_cookie.js
@@ -3,8 +3,8 @@
 //TG群:https://t.me/joinchat/wH4Ks3mT6mxiMDg1
 
 let cookies = [
-  '',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
-  '',//账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
+  '',//账号一ck,例:deviceid_pdj_jd=xxx-xxx-xxx;o2o_m_h5_sid=xxx-xxx-xxx;
+  '',//账号二ck,例:deviceid_pdj_jd=xxx-xxx-xxx2;o2o_m_h5_sid=xxx-xxx-xxx2;如有更多,依次类推
 ]
 // 判断环境变量里面是否有京东ck
 if (process.env.JDDJ_COOKIE) {
