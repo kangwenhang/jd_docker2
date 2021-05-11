ce2dd93d72ee4a66bdc760101710e49abf643f50
Ca11back <steliske@gmail.com>
2021-05-11 11:18:29 +0800

    add ck env var:JDDJ_COOKIE

--- jddj_cookie.js
+++ jddj_cookie.js
@@ -3,7 +3,27 @@
 //TG群:https://t.me/joinchat/wH4Ks3mT6mxiMDg1
 
 let cookies = [
-  'ck1',
-  'ck2'
-];
-module.exports = cookies;
+  '',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
+  '',//账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
+]
+// 判断环境变量里面是否有京东ck
+if (process.env.JDDJ_COOKIE) {
+  if (process.env.JDDJ_COOKIE.indexOf('&') > -1) {
+    cookies = process.env.JDDJ_COOKIE.split('&')
+  } else if (process.env.JDDJ_COOKIE.indexOf('\n') > -1) {
+    cookies = process.env.JDDJ_COOKIE.split('\n')
+  } else {
+    cookies = [process.env.JDDJ_COOKIE]
+  }
+}
+if (JSON.stringify(process.env).indexOf('GITHUB')>-1) {
+  console.log(`请勿使用github action运行此脚本,无论你是从你自己的私库还是其他哪里拉取的源代码，都会导致我被封号\n`)
+  !(async () => {
+    await require('./sendNotify').sendNotify('提醒', `请勿使用github action、滥用github资源会封我仓库以及账号`)
+    await process.exit(0)
+  })()
+}
+cookies = [...new Set(cookies.filter(item => !!item))]
+console.log(`\n====================共${cookies.length}个京东账号Cookie=========\n`)
+
+module.exports = cookies
