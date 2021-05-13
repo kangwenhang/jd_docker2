80d6391844b8e2f73803541f25ac602bc18def84
Ca11back <steliske@gmail.com>
2021-05-13 14:29:07 +0800

    fix typo

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ exports.main_handler = async (event, context, callback) => {
         for (const v of event["Message"].split("&")) {
             console.log(v);
             if (SOURCE_URL) { //不允许直链!!!不允许直链!!!不允许直链!!!
-                const request = require('request');
+                const request = require('request')
                 request(`${SOURCE_URL}${v}.js`, function(error, response, body) {
                     eval(response.body)
                 })
