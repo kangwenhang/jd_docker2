e2d7394f89b3c910146cf6a7845cbfdfc7e91548
passerby-b <40885345+passerby-b@users.noreply.github.com>
2021-05-24 14:26:51 +0800

    Update jddj_fruit.js

--- jddj_fruit.js
+++ jddj_fruit.js
@@ -16,7 +16,7 @@ cron "10 0,8,11,17 * * *" script-path=https://raw.githubusercontent.com/passerby
 */
 
 let isNotify = true;//是否通知,仅限nodejs
-let ckPath = './jddj_cookie.js';//ck路径
+let ckPath = './jddj_cookie.js';//ck路径,环境变量:JDDJ_CKPATH
 
 const $ = new API("jddj_fruit");
 try {
