d3c27dacf6ad9cd67c3721f3d876cbcd667fb167
passerby-b <40885345+passerby-b@users.noreply.github.com>
2021-05-24 14:04:57 +0800

    Update jddj_plantBeans.js

--- jddj_plantBeans.js
+++ jddj_plantBeans.js
@@ -13,7 +13,6 @@ TG群:https://t.me/passerbyb2021
 [Script]
 cron "0 0 * * *" script-path=https://raw.githubusercontent.com/passerby-b/JDDJ/main/jddj_plantBeans.js,tag=京东到家鲜豆庄园
 
-
 */
 
 const $ = new API("jddj_plantBeans");
