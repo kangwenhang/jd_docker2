276a164817002e1039729f8de2594b2e18727dc7
WHFo <996579747@qq.com>
2021-05-30 20:16:50 +0800

    Update jddj_cookie.js

--- jddj_cookie.js
+++ jddj_cookie.js
@@ -11,7 +11,7 @@ if (process.env.JDDJ_COOKIE) {
   if (process.env.JDDJ_COOKIE.indexOf(',') > -1) {
     cookies = process.env.JDDJ_COOKIE.split(',')
   } else if (process.env.JDDJ_COOKIE.indexOf('&') > -1) {
-    cookies = process.env.JDDJ_COOKIE.split('\n')
+    cookies = process.env.JDDJ_COOKIE.split('&')
   }else if (process.env.JDDJ_COOKIE.indexOf('\n') > -1) {
     cookies = process.env.JDDJ_COOKIE.split('\n')
   }else {
