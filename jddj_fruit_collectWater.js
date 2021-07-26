032d7b3246993dea554807160d16317e4eea6c20
passerby-b <40885345+passerby-b@users.noreply.github.com>
2021-07-26 17:25:36 +0800

    Update jddj_fruit_collectWater.js

--- jddj_fruit_collectWater.js
+++ jddj_fruit_collectWater.js
@@ -13,7 +13,7 @@ TG群:https://t.me/passerbyb2021
 
 //================Loon==============
 //[Script]
-//cron "*/5 * * * *" script-path=https://raw.githubusercontent.com/passerby-b/JDDJ/main/jddj_fruit_collectWater.js,tag=京东到家果园水车收水滴
+//cron "* */1 * * *" script-path=https://raw.githubusercontent.com/passerby-b/JDDJ/main/jddj_fruit_collectWater.js,tag=京东到家果园水车收水滴
 //
 
 const $ = new API("jddj_fruit_collectWater");
