dd2f1827ee53da10932d9b5ec6ea3b6badcdc3ea
passerby-b <40885345+passerby-b@users.noreply.github.com>
2021-08-20 16:23:19 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+/*
+原生nodejs运行方式,不需要面板,下面配置好脚本和运行时间,在pm2挂在该主要程序脚本,
+安装pm2命令:npm install pm2 -g
+挂在主程序命令:pm2 start main.js --name myserver
+
+*/
+var cronJob = require("cron").CronJob;
+
+//在此处配置定时和脚本
+cron('*/20 * * * * *', './111.js');
+cron('*/20 * * * * *', './222.js');
+
+
+
+function cron(time, file) {
+    new cronJob(time, () => {
+        try {
+            delete require.cache[require.resolve(file)];
+            require(file);
+        } catch (error) {
+            console.log('\r\n ' + file + "_erro:" + error);
+        }
+    }, null, true);
+}
