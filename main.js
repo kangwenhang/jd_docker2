955cc5c5471396d316f399a6e4a91c191126b8ba
passerby-b <40885345+passerby-b@users.noreply.github.com>
2021-08-20 16:50:32 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,21 +4,11 @@
 挂载主程序命令:pm2 start main.js --name myserver
 
 */
+var request = require('sync-request');
 var cronJob = require("cron").CronJob;
 
 //在此处配置定时和脚本
-cron('*/20 * * * * *', './111.js');
-cron('*/20 * * * * *', './222.js');
+cron('0 0 8 * * *', 'https://raw.githubusercontents.com/passerby-b/JDDJ/main/jddj_fruit.js');//远程脚本
+cron('0 0 8 * * *', './jddj_fruit.js');//本地脚本
 
-
-
-function cron(time, file) {
-    new cronJob(time, () => {
-        try {
-            delete require.cache[require.resolve(file)];
-            require(file);
-        } catch (error) {
-            console.log('\r\n ' + file + "_erro:" + error);
-        }
-    }, null, true);
-}
+function cron(time,file){new cronJob(time,()=>{try{if(file.indexOf('http://')>-1||file.indexOf('https://')>-1){let res=request('get',file);if(res.getBody('utf8'))eval(res.getBody('utf8'))}else{delete require.cache[require.resolve(file)];require(file)}}catch(error){console.log('\r\n '+file+"_erro:"+error)}},null,true)}
