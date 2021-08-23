33dbbc55a303818bc2a775b53d60773999b2c881
cdle <798731886@qq.com>
2021-08-23 20:42:20 +0800

    x

--- controllers/login.go
+++ controllers/login.go
@@ -302,8 +302,8 @@ func CheckLogin(token, cookie, okl_token string) (string, *models.JdCookie) {
 	case 258: //务异常，请稍后重试
 		return "", nil
 	case 264: //出错了，请退出重试
-		JdCookieRunners.Delete(token)
-		return sth.Message, nil
+		// JdCookieRunners.Delete(token)
+		// return sth.Message, nil
 	default:
 		JdCookieRunners.Delete(token)
 		fmt.Println(sth)
