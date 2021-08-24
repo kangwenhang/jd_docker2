254bcf408799ffd3228f6b7f59d549172ae889f3
cdle <798731886@qq.com>
2021-08-24 14:46:48 +0800

    x

--- models/command.go
+++ models/command.go
@@ -161,7 +161,7 @@ var codeSignals = []CodeSignal{
 				if coin < 0 {
 					return "许愿币不足以查看UserAgent。"
 				}
-				sender.Reply("查看一次扣5个心愿币。")
+				sender.Reply("查看一次扣5个许愿币。")
 				RemCoin(sender.UserID, 5)
 			}
 			return ua
