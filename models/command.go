1dbbddc88d3f78d3539e1a521b8a75ea3f38288c
cdle <798731886@qq.com>
2021-08-25 18:22:01 +0800

    x

--- models/command.go
+++ models/command.go
@@ -253,10 +253,15 @@ var codeSignals = []CodeSignal{
 		Command: []string{"许愿", "wish", "hope", "want"},
 		Handle: func(sender *Sender) interface{} {
 			b := GetCoin(sender.UserID)
+			w := sender.JoinContens()
+			if w == "" {
+				return "请说出你的愿望。"
+			}
 			if b < 25 {
 				return "许愿币不足，需要25个许愿币。"
 			}
-			(&JdCookie{}).Push(fmt.Sprintf("%d许愿%s，许愿币余额%d。", sender.UserID, sender.JoinContens(), b))
+
+			(&JdCookie{}).Push(fmt.Sprintf("%d许愿%s，许愿币余额%d。", sender.UserID, w, b))
 			return fmt.Sprintf("收到许愿，已扣除25个许愿币，余额%d。", RemCoin(sender.UserID, 25))
 		},
 	},
